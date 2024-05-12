a77019fdd61c51a63cf91d952571aa030b802933
kenancafer <kenancafer27@gmail.com>
2024-05-12 22:23:34 +0300

    w-filter

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Table from "react-bootstrap/Table";
 import { nanoid } from "nanoid";
 import { useState } from "react";
 import IconButton from "./components/IconButton";
+import Fuse from "fuse.js";
 function App() {
   const shops = [
     {
@@ -34,6 +35,10 @@ function App() {
   const [input, setInput] = useState("");
   const [productShop, setproductShop] = useState("");
   const [productCategory, setproductCategory] = useState("");
+  const [filteredName, setFilteredName] = useState("");
+  const [filteredShopId, setFilteredShopId] = useState("");
+  const [filteredCategoryId, setFilteredCategory] = useState("");
+  const [filteredStatus, setFilteredStatus] = useState("");
 
   const addShop = () => {
     const productName = input;
@@ -46,7 +51,26 @@ function App() {
     setProducts([...products, product]);
     setInput("");
   };
-
+  const filteredProducts = products.filter((product) => {
+    let result = true;
+    const fuse = new Fuse(products, {
+      keys: ["name"],
+    });
+    const res = fuse.search(filteredName);
+    if (filteredName !== "" && !res.find((r) => r.item.id === product.id)) {
+      result = false;
+    }
+    if (filteredStatus !== "reset" && !!product.isBought !== filteredStatus) {
+      result = false;
+    }
+    if (filteredShopId !== "" && product.shop !== filteredShopId) {
+      result = false;
+    }
+    if (filteredCategoryId !== "" && product.shop !== filteredCategoryId) {
+      result = false;
+    }
+    return result;
+  });
   return (
     <>
       <Container className="my-5 px-5">
@@ -88,6 +112,87 @@ function App() {
             Ekle
           </Button>
         </InputGroup>
+
+        {/* Filtreleme Alanı */}
+
+        <InputGroup className="mb-3">
+          <Form.Control
+            placeholder="Filtrelemek istediğin ürün"
+            value={filteredName}
+            onChange={(e) => {
+              setFilteredName(e.target.value);
+            }}
+          />
+          <Form.Group
+            onChange={(e) => {
+              const val = e.target.value;
+
+              setFilteredStatus(
+                val === "reset" ? val : val === "true" ? true : false
+              );
+            }}
+            className="d-flex p-2 mx-1"
+          >
+            <Form.Check // prettier-ignore
+              value={true}
+              className="mx-3"
+              name="group1"
+              type={"radio"}
+              id={`default-radio-1`}
+              label={`alınmış`}
+            />
+            <Form.Check // prettier-ignore
+              value={false}
+              name="group1"
+              className="mx-3"
+              type={"radio"}
+              id={`default-radio-2`}
+              label={`alınmamış`}
+            />
+            <Form.Check // prettier-ignore
+              value={"reset"}
+              name="group1"
+              className="mx-3"
+              type={"radio"}
+              id={`default-radio-3`}
+              label={`sıfırla`}
+            />
+          </Form.Group>
+          <Form.Select
+            style={{ maxWidth: "200px" }}
+            value={filteredCategoryId}
+            onChange={(e) => setFilteredCategory(e.target.value)}
+          >
+            <option value={""}>Kategori filtresi</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.name}>
+                {category.name}
+              </option>
+            ))}
+            <Form.Check
+              checked={filteredStatus}
+              onChange={(e) => {
+                setFilteredStatus(e.target.checked);
+              }}
+              type={"checkbox"}
+              id={`default-checkbox`}
+              label={`status`}
+            />
+          </Form.Select>
+          <Form.Select
+            style={{ maxWidth: "200px" }}
+            value={filteredShopId}
+            onChange={(e) => setFilteredShopId(e.target.value)}
+          >
+            <option value={""}>Market filtresi</option>
+            {shops.map((shop) => (
+              <option key={shop.id} value={shop.name}>
+                {shop.name}
+              </option>
+            ))}
+          </Form.Select>
+        </InputGroup>
+
         <div>
           <Table striped bordered className="mt-5">
             <thead>
@@ -100,7 +205,7 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              {products.map((product, index) => (
+              {filteredProducts.map((product, index) => (
                 <tr
                   key={index}
                   onClick={() => {
